17fd9d13a425549ee2a9c87c0b7fa4387e0908b3
Dawid Kotarba <dawidkotarba>
2018-08-19 01:48:30 +0200

    add several duplicate methods in indbound endpoint

--- src/main/java/com/dawidkotarba/springIntegrationPlayground/endpoint/IndboundEndpoint.java
+++ src/main/java/com/dawidkotarba/springIntegrationPlayground/endpoint/IndboundEndpoint.java
@@ -11,8 +11,19 @@ public class IndboundEndpoint {
 
     private static final Logger LOG = LoggerFactory.getLogger(IndboundEndpoint.class);
 
-    public void post(Message<Customer> msg) {
-        LOG.info("Login: {}", msg.getPayload().getLogin());
+    public Message<Customer> post1(Message<Customer> msg) {
+        LOG.info("Login [1]: {}", msg.getPayload().getLogin());
+        return msg;
+    }
+
+    public Message<Customer> post2(Message<Customer> msg) {
+        LOG.info("Login [2]: {}", msg.getPayload().getLogin());
+        return msg;
+    }
+
+    public Message<Customer> post3(Message<Customer> msg) {
+        LOG.info("Login [3]: {}", msg.getPayload().getLogin());
+        return msg;
     }
 
 }
